ba1e76e9dba2560f68c0024f8b9422b38437d444
KakanspelarRN <kakanspelar@outlook.com>
2025-09-05 23:33:13 +0200

    Initial commit

---
+++ src/client/java/kakanspelar/demon/DemonsArsenalDataGenerator.java
@@ -0,0 +1,11 @@
+package kakanspelar.demon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DemonsArsenalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/kakanspelar/demon/DemonsArsenalClient.java
@@ -0,0 +1,10 @@
+package kakanspelar.demon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemonsArsenalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
